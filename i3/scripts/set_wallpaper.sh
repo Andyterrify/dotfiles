eb01c380857f990fe02032267afa59007bd01428
Andrei Vasile <andreibvasile@gmail.com>
2024-03-11 20:27:03 +0000

    removed old config

--- i3/scripts/set_wallpaper.sh
+++
@@ -1,3 +0,0 @@
-export DISPLAY=":0.0"
-/usr/bin/nitrogen --head=0 --set-zoom-fill --random /home/avasile/Pictures/Wallpapers
-/usr/bin/nitrogen --head=1 --set-zoom-fill --random /home/avasile/Pictures/Wallpapers
