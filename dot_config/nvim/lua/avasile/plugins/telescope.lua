80ce6ee81d1bf2d02f8ee86caed6ac3295b736f7
Andrei Vasile <andreibvasile@gmail.com>
2026-01-17 21:52:03 +0000

    nvim telescope improvement

--- dot_config/nvim/lua/avasile/plugins/telescope.lua
+++ dot_config/nvim/lua/avasile/plugins/telescope.lua
@@ -87,7 +87,7 @@ return {
 
 	-- FZF native for better performance
 	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+	}
 }
