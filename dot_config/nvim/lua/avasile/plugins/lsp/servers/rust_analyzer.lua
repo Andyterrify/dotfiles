c2912f30c22c347c2733df1c46e8e8950fdc68df
Andrei Vasile <andreibvasile@gmail.com>
2026-01-18 14:19:11 +0000

    removing neovim from dotfiles

--- dot_config/nvim/lua/avasile/plugins/lsp/servers/rust_analyzer.lua
+++
@@ -1,7 +0,0 @@
-return {
-	cmd = { "rust-analyzer" },
-	filetypes = { "rust" },
-	root_dir = function(fname)
-		return vim.fs.root(fname, { "Cargo.toml", ".git" })
-	end,
-}
--- dot_config/nvim/lua/avasile/plugins/lsp/servers/tailwindcss.lua
+++
@@ -1,7 +0,0 @@
-return {
-	cmd = { "tailwindcss-language-server", "--stdio" },
-	filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-	root_dir = function(fname)
-		return vim.fs.root(fname, { "tailwind.config.js", "tailwind.config.ts", ".git" })
-	end,
-}
